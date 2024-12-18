02a2ee6cd934e3dc83be4595fafeb6893b33738b
Alvarezpro87 <alvareznuvem@gmail.com>
2024-12-18 09:57:28 -0300

    Subindo alteração do dockerfile do backend para deploy na nuvem

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b01ac0a501301913e9cae8e2120ca3407119b3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e60c9dbce378d012a4aa5cbc41dd5816144d902")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
